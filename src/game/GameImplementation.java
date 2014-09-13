5d0e5a4868fd815cfc82dc521ae3680a3724036d
Shantanu Alshi <shantanuals@gmail.com>
2014-09-13 17:51:08 +0800

    Added methods to generate board, treasures and gameplay

--- src/game/GameImplementation.java
+++ src/game/GameImplementation.java
@@ -3,23 +3,56 @@ package game;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
+import java.util.Random;
 
 import server.WaitConnect;
 
 public class GameImplementation extends UnicastRemoteObject implements GameMethod{
 
-	GameInfo gameInfo = GameInfo.NotStarted;
-	
+	private int boardSize;
+	private int numberOfTreasures;
+	private int[][] gameBoard;
+	private int[][] playersLocation;
 	int id = 0;
+	
+	GameInfo gameInfo = GameInfo.NotStarted;
 	
 	private static final long serialVersionUID = -4933868291603601249L;
 	
 	String[] msg = new String[2];
 
+	//Randomly place treasures on the board.
+	private void placeTreasures(int numberOfTreasures){
+		int randomX = new Random().nextInt(numberOfTreasures);
+		int randomY = new Random().nextInt(numberOfTreasures);
+			
+		for(int i=0;i<numberOfTreasures;i++){
+			//Treasures have values as non-zero integers on the board.
+			gameBoard[randomX][randomY] = i;
+		}
+	}
+	
+	private void prepareGameBoard(){
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				if(gameBoard[i][j]==0)
+					System.out.println("_");
+				else
+					System.out.println("gameBoard[i][j]");
+			}
+		}
+	}
+
 	
 	public GameImplementation() throws RemoteException {
 		super();
-		// TODO Auto-generated constructor stub
+		//Set variables- 
+				this.boardSize = boardSize;
+				this.numberOfTreasures = numberOfTreasures;
+				gameBoard = new int[boardSize][boardSize];
+				playersLocation = new int[boardSize][boardSize];
+				//Place treasures on the board
+				placeTreasures(numberOfTreasures);
 	}
 	
 	
@@ -82,6 +115,47 @@ public class GameImplementation extends UnicastRemoteObject implements GameMetho
 		
 		
 	}
+	
+//	This function will modify the current position of the user
+	public Position play(Player thisPlayer, char userInput){
+		//Get the current player's position
+		Position newPosition = thisPlayer.getPosition();
 		
+		//Get his X and Y cordinates
+		int xPos = newPosition.getxPos();
+		int yPos = newPosition.getyPos();
+		
+		//Move according to client input
+		switch (Character.toLowerCase(userInput)) {
+		
+			case 's':
+				//MoveLeft - Xpos-- and YPos same 
+				newPosition.setxPos(xPos--);
+				break;
+			
+			case 'd':
+				//MoveDown - YPos-- and Xpos same
+				newPosition.setyPos(yPos--);
+				break;
+			
+			case 'e':
+				//MoveUp - YPos++ and XPos same
+				newPosition.setyPos(yPos++);
+				break;
+			
+			case 'f':
+				//MoveRight - XPos++ and YPos same
+				newPosition.setxPos(xPos++);
+				break;
+			
+			default:
+				System.out.println("Invalid Input!");
+			}
+		
+		return newPosition;
+		
+	}
+	
+	
 			
 }
